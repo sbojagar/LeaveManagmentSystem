882d92898c6a96c29ed96a29a3fc36fe0e15eeed
shoeb <shoeb@MSI>
2024-10-04 22:09:20 +0530

    Add project files.

---
+++ LeaveManagmentSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using LeaveManagmentSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagmentSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Shoeb",
+                DOB = new DateTime(1985,06,23)
+            };
+            return View(data);
+        }
+    }
+}
