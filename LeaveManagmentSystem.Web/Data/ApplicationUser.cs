8c96b5a9dfb77edbcc5fa807316805d8bf1650ac
Shoeb B <shoebbojagar@outlook.com>
2025-09-05 18:58:57 +0530

    Fixed Custom User Registration Issue by scalfolding Identiy Register

--- LeaveManagmentSystem.Web/Data/ApplicationUser.cs
+++ LeaveManagmentSystem.Web/Data/ApplicationUser.cs
@@ -4,9 +4,9 @@ namespace LeaveManagmentSystem.Web.Data
 {
     public class ApplicationUser:IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        //public string Email { get; set; }
-        public DateOnly  DateOfBirth { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            //public string Email { get; set; }
+            public DateOnly  DateOfBirth { get; set; }
     }
 }
